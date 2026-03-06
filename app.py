447060e99533bb6f05f2123e32157aaf8b27d79f
Gustavo Graciano <gracianogustavo74@gmail.com>
2026-03-05 19:17:24 -0600

    Fixing the bug so the game chooses a number in range based on difficulty.

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if "history" not in st.session_state:
 st.subheader("Make a guess")
 
 st.info(
-    f"Guess a number between 1 and 100. "
+    f"Guess a number between {low} and {high}. "
     f"Attempts left: {attempt_limit - st.session_state.attempts}"
 )
 
@@ -107,7 +107,7 @@ with col3:
 
 if new_game:
     st.session_state.attempts = 0
-    st.session_state.secret = random.randint(1, 100)  #FIX ME: Secret number range does not change with difficulty
+    st.session_state.secret = random.randint(low, high)
     st.success("New game started.")
     st.rerun()
 
